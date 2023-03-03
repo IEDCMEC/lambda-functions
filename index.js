1e392378f0dc0b001c71671b8492c11c43403aca
Aldrin Jenson <aldrinjenson@gmail.com>
2023-03-03 07:38:38 +0530

    minor update

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/gettodo", async (_, res) => {
 
 app.get("/", (_, res) => {
   console.log("yo");
-  res.send({ working: "yes bro" });
+  res.send({ working: "yes bro", status: "alive and kicking" });
 });
 
 if (process.env.isDev) {
